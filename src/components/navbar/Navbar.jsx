02bbc947913066fd4fe42d0fe41cd95a6823a56b
Hlnaji <114623203+hlnaji@users.noreply.github.com>
2024-01-22 22:50:11 +0100

    fixing icons not showing

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -17,10 +17,10 @@ const Navbar = () => {
         </motion.span>
         <div className="social">
           <a href="https://www.facebook.com/people/%D8%A7%D9%84%D8%AB%D8%A7%D9%86%D9%88%D9%8A%D8%A9-%D8%A7%D9%84%D8%AA%D8%A3%D9%87%D9%8A%D9%84%D9%8A%D8%A9-20-%D8%BA%D8%B4%D8%AA-%D8%A7%D9%84%D9%85%D8%AF%D9%8A%D8%B1%D9%8A%D8%A9-%D8%A7%D9%84%D8%A5%D9%82%D9%84%D9%8A%D9%85%D9%8A%D8%A9-%D8%B7%D8%A7%D9%86%D8%B7%D8%A7%D9%86/100087363576820/">
-            <img src="/facebook.png" alt="" />
+            <img src="../images/facebook.png" alt="" />
           </a>
           <a href="https://maps.app.goo.gl/KWj66LogRm1JPiBf6">
-            <img src="/map.png" alt="" />
+            <img src="../images/map.png" alt="" />
           </a>
         </div>
       </div>
